96fcc3d11579888870e755713d507463d9c1ba6e
awsomeaw <dratfatgatcat@yahoo.com>
2025-11-18 13:21:33 -0500

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import ui
 def new_game():
     game.start_new_game()
     ui.show_message("")
-    ui.show_game(game.dealer_hand, game.player_hand)
+    ui.show_game(game.dealer_hand, game.player_hand, hidden=True)
     ui.show_score(game.calculate_hand_value(game.player_hand))
     ui.enable_buttons()
 
@@ -13,7 +13,7 @@ def result():
 
     ui.show_message(status)
     ui.disable_buttons()
-    ui.show_money(game.money_count(game.check_winner()))
+    ui.show_money(game.money_count(status))
 
 def hit():
     score = game.player_hit()
@@ -30,9 +30,9 @@ def stand():
 
 
 def double_bet():
-        game.player_money = game.player_money - 50
-        game.is_double_bet_called = True
-        ui.disable_bet_button()
+    game.player_money = game.player_money - 50
+    game.is_double_bet_called = True
+    ui.disable_bet_button()
 
 # Create the window
 hit_btn, stand_btn, new_btn, bet_btn = ui.create_window()
